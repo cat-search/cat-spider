7928a502e4f3d32d1dfc47ff9d913dc843ef4f8e
Valeriy <lev.370x@gmail.com>
2025-04-07 09:24:07 +0200

    settings.py: cat conn string added

--- src/common/settings.py
+++ src/common/settings.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):
     vk_db_conn_str_cms: str           = 'postgresql://postgres:postgres@localhost:5433/cms'
     vk_db_conn_str_filestorage: str   = 'postgresql://postgres:postgres@localhost:5433/filestorage'
     vk_db_conn_str_lists: str         = 'postgresql://postgres:postgres@localhost:5433/lists'
+    cat_db_conn_str: str              = 'postgresql://postgres:postgres@localhost:5433/catsearch'
     # mongo_host: str                 = '127.0.0.1'
     # mongo_port: int                 = 27017
     # mongo_db_name: str              = 'raw'
