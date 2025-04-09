5bc8176af45a1654069c061f7492039d220cb73b
Valeriy <lev.370x@gmail.com>
2025-04-09 08:57:59 +0200

    import_page added

---
+++ src/tasks/pg/import_page.py
@@ -0,0 +1,90 @@
+from collections import defaultdict
+
+from bson.binary import UuidRepresentation
+from bson.codec_options import CodecOptions
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+from sqlalchemy import select, func, cast, Text
+from sqlalchemy.dialects.postgresql import UUID
+
+from src.common.db import DbConnManager
+from src.common.log import logger
+from src.common.mongo import init_mongo
+from src.common.settings import settings
+from src.common.utils import get_stats
+from src.models.vk_cms import Site, SiteServiceObject, Page, SiteStatus
+
+
+def import_page(stats: dict) -> None:
+    """
+    Import pages_page table from DB.
+    """
+    with DbConnManager(settings.vk_db_conn_str_cms) as conn:
+        # Query pages_page + site_service_object
+        query = select(
+            Page.id.label('page_id'),  # id страницы
+            Page.name,
+            Page.body,                                    # содержимое страницы
+            Page.views_count,                             # Кол-во просмотров
+            Page.created_at,
+            Page.created_by_id,
+            Page.updated_at,
+            Page.updated_by_id,
+            SiteServiceObject.site_id,
+            SiteServiceObject.type                        # page all
+            # TODO: We need to add link to the site here
+        ).join(
+            SiteServiceObject,
+            SiteServiceObject.external_id == cast(Page.id, Text),
+        ).where(
+            SiteServiceObject.site_id.in_(settings.site_ids),  # Our sites
+        ).execution_options(stream_results=True)          # Streaming for chunking
+
+        # Initialize MongoDB client, db, collection
+        coll = init_mongo(settings.mongo_collection_page)
+
+        # Iterate over rows
+        for i, row in enumerate(conn.execute(query).yield_per(settings.chunk_size)):
+            logger.info(f"{i}, {row.page_id}, {row.name}")
+            try:
+                doc = {
+                    # _id in MongoDB - unique identifier
+                    '_id'           : row.page_id,          # We will use page_id as _id
+                    'name'          : row.name,
+                    'body'          : row.body,             # содержимое страницы
+                    'views_count'   : row.views_count,      # Кол-во просмотров
+                    'created_at'    : row.created_at,
+                    'created_by_id' : row.created_by_id,
+                    'updated_at'    : row.updated_at,
+                    'updated_by_id' : row.updated_by_id,
+                    'site_id'       : row.site_id,
+                    'type'          : row.type,             # page all
+                }
+
+                # TODO: Here we can insert into vector db directly
+
+                # Insert document into MongoDB
+                coll.insert_one(doc)
+                stats['site']['inserted'] += 1
+            # Duplicated _id (site_id) in MongoDB
+            except DuplicateKeyError:
+                stats['site']['duplicate_key_error'] += 1
+
+
+@logger.catch(reraise=True)
+def main():
+    logger.info("Fetcher started")
+
+    # Statistics
+    stats: dict = {
+        'site': defaultdict(int),
+    }
+
+    import_page(stats)
+
+    logger.info(get_stats(stats))
+    logger.info("Fetcher finished")
+
+
+if __name__ == '__main__':
+    main()
