bd6fb6a1984b7607e511dd6466db65c94b8fb2b5
Valeriy <lev.370x@gmail.com>
2025-04-10 18:46:17 +0200

    marqo

--- src/common/vectordb.py
+++ src/common/vectordb.py
@@ -11,22 +11,30 @@ def init_marqo(
 ) -> marqo.Client:
     # Create a Marqo client
     logger.info(f"Initializing marqo client: {settings.marqo_url} ...")
-    mq = marqo.Client(
+    mq: marqo.Client = marqo.Client(
         url=settings.marqo_url,
-        main_user=settings.marqo_user,
-        main_password=settings.marqo_password,
+        # main_user=settings.marqo_user,
+        api_key=settings.marqo_api_key,
+        # main_password=settings.marqo_password,
     )
 
     if delete_index:  # Housekeeping - Delete the index if it already exists
         try:
             mq.index(index).delete()
-        except:
+        except Exception as e:
             pass
 
+    results = mq.get_indexes().get('results', [])
+    logger.info(f"Marqo indexes: {results}")
+    indexes = set(
+        [item.get('indexName') for item in results]
+    )
+
     # Create an index if it doesn't exist
-    if check_index := mq.index(index):
+    if check_index := index in indexes:
         logger.info(f"Marqo index exists: {index}")
     if create_index and not check_index:
-        logger.info(f"Creating marqo index: {index} ...")
+        logger.info(msg := f"Creating marqo index: {index} ...")
         mq.create_index(index, model="hf/e5-base-v2")
+        logger.info(f"{msg} done")
     return mq
