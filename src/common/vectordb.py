0e0b736ab8046f6098cf7a49d60d5cd499acb974
Valeriy <lev.370x@gmail.com>
2025-04-11 00:55:37 +0200

    index settings

--- src/common/vectordb.py
+++ src/common/vectordb.py
@@ -1,3 +1,5 @@
+from pprint import pformat
+
 import marqo
 from src.common.settings import settings
 from src.common.log import logger
@@ -33,8 +35,12 @@ def init_marqo(
     # Create an index if it doesn't exist
     if check_index := index in indexes:
         logger.info(f"Marqo index exists: {index}")
+
     if create_index and not check_index:
         logger.info(msg := f"Creating marqo index: {index} ...")
-        mq.create_index(index, model="hf/e5-base-v2")
+        index_settings = settings.marqo_index_settings
+        index_settings['model'] = settings.marqo_model
+        logger.info(f"settings:\n{pformat(settings.marqo_index_settings)}")
+        mq.create_index(index, settings_dict=index_settings)
         logger.info(f"{msg} done")
     return mq
