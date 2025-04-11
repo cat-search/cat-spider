160e7e6787c7c75f140269bec904bd296d3c9db7
Valeriy <lev.370x@gmail.com>
2025-04-11 14:06:34 +0200

    marqo

--- src/common/settings.py
+++ src/common/settings.py
@@ -44,6 +44,7 @@ class Settings(BaseSettings):
     marqo_user: str                     = "admin"
     marqo_api_key: str                  = ""
     marqo_model: str                    = "hf/e5-base-v2"
+    # marqo_model: str                    = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
     marqo_index_page: str               = "page_20250409_valer"
     marqo_index_settings: dict          = {
         "textPreprocessing": {
