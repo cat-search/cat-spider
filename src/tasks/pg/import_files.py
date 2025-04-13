42ecdf49210b6f9ad24fe1fbaae65283c42cd8bf
Valeriy <lev.370x@gmail.com>
2025-04-13 17:18:06 +0200

    import_files: pdf, xlsx

--- src/tasks/pg/import_files.py
+++ src/tasks/pg/import_files.py
@@ -1,19 +1,15 @@
 import subprocess
 from collections import defaultdict
-from pprint import pformat
 
-import pandas as pd
 import PyPDF2
-from pymongo.errors import DuplicateKeyError
+import pandas as pd
 from sqlalchemy import select
 
 from src.common.db import DbConnManager, get_sites, get_storage_object
 from src.common.log import logger
 from src.common.mongo import init_mongo, mongo_insert
 from src.common.settings import settings
-from src.common.utils import (get_stats, make_storage_url, prepare_doc,
-                              write_text_file)
-from src.common.vectordb import init_marqo
+from src.common.utils import (get_stats, make_storage_url, write_text_file)
 from src.models.cat_meta import SpiderFile, Status
 from src.models.vk_filestorage import StorageObject
 
@@ -51,8 +47,29 @@ def parse_doc(file: SpiderFile, stats: dict) -> str:
             )
         )
         raise AssertionError(msg)
-    
+
+    text_len = len(full_text)
+    stats['text_len'] = text_len
+    if text_len > 0:
+        stats['parsed'] = 1
+    logger.info(f"{msg} done")
     return full_text
+    # Split text into pages using form feed character
+    # pages = full_text.split('\x0c')     # It's not working!!!
+    # stats['pages'] = len(pages)
+    #
+    # structured_pages = []
+    # for page_num, page_content in enumerate(pages, start=1):
+    #     # Clean and split into paragraphs
+    #     paragraphs = [p.strip() for p in page_content.split('\n\n') if p.strip()]
+    #
+    #     if paragraphs:  # Skip empty pages
+    #         structured_pages.append({
+    #             'page_number': page_num,
+    #             'paragraphs': paragraphs
+    #         })
+    #         stats[f"page_{page_num}"] = len(paragraphs)
+
 
 def parse_pdf(file: SpiderFile, stats: dict) -> str:
     """
@@ -77,10 +94,19 @@ def parse_pdf(file: SpiderFile, stats: dict) -> str:
                 if text:
                     full_text += f"Page {page_num + 1}:\n{text}\n\n"
             if not full_text:
-                raise AssertionError("No text extracted from PDF.")
+                # raise AssertionError("No text extracted from PDF.")
+                logger.info(f"Text length: {len(full_text)}")
     except Exception as e:
         logger.error(msg := f"Error parsing PDF file: {e}")
         raise AssertionError(msg)
+
+    text_len = len(full_text)
+    stats['text_len'] = text_len
+    if text_len > 0:
+        stats['parsed'] = 1
+    logger.info(f"{msg} done")
+    return full_text
+
 
 def parse_excel(file: SpiderFile, stats: dict) -> str:
     """
@@ -107,25 +133,10 @@ def parse_excel(file: SpiderFile, stats: dict) -> str:
         logger.error(msg := f"Error parsing Excel file: {e}")
         raise AssertionError(msg)
 
-    return full_text
-
-#    return full_text
-    # Split text into pages using form feed character
-    # pages = full_text.split('\x0c')     # It's not working!!!
-    # stats['pages'] = len(pages)
-    #
-    # structured_pages = []
-    # for page_num, page_content in enumerate(pages, start=1):
-    #     # Clean and split into paragraphs
-    #     paragraphs = [p.strip() for p in page_content.split('\n\n') if p.strip()]
-    #
-    #     if paragraphs:  # Skip empty pages
-    #         structured_pages.append({
-    #             'page_number': page_num,
-    #             'paragraphs': paragraphs
-    #         })
-    #         stats[f"page_{page_num}"] = len(paragraphs)
-
+    text_len = len(full_text)
+    stats['text_len'] = text_len
+    if text_len > 0:
+        stats['parsed'] = 1
     logger.info(f"{msg} done")
     return full_text
 
@@ -167,20 +178,14 @@ def parse(stats: dict) -> int:
 
             # 2. Parse file
             if file_type == 'doc':
+                # continue
                 data = parse_doc(file, stats['file'])   # Parse .doc file
-                if data:
-                    stats['file'][file_name]['parsed'] = 1
-                continue
             elif file_type == 'pdf':
-                data = parse_pdf(file.target_path)  # Parse PDF file
-                if data:
-                    stats['file'][file_name]['parsed'] = 1
-                continue
+                # continue
+                data = parse_pdf(file, stats['file'])  # Parse PDF file
             elif file_type == 'xlsx':
+                # continue
                 data = parse_excel(file, stats['file'])  # Parse Excel file
-                if data:
-                    stats['file'][file_name]['parsed'] = 1
-                continue
             else:
                 raise AssertionError(
                     f"Unknown file type: {file_type}, {file.storage_object_name}"
@@ -210,31 +215,6 @@ def parse(stats: dict) -> int:
             # 5. Insert doc into MongoDB
             mongo_insert(coll, doc, stats)
 
-            # for page in data:
-            #     for para_num, paragraph in enumerate(page['paragraphs']):
-            #         doc = {
-            #             '_id': f"{file.storage_object_id}-{page}-{paragraph}",
-            #             'storage_object_id': file.storage_object_id,
-            #             'name': file.storage_object_name,
-            #             'site_id': file.storage_object_site_id,
-            #             'site_name': sites[file.storage_object_site_id],
-            #             'data': paragraph,
-            #             'page': page['page_number'],
-            #             'paragraph': para_num,
-            #             'link': make_storage_url(file.storage_version_link),
-            #         }
-            #         stats['page']['read'] += 1
-            #         # Insert into VectorDB
-            #         prepared_doc = prepare_doc(doc)
-            #         result = idx.add_documents(
-            #             [prepared_doc],
-            #             tensor_fields=['name', 'body']
-            #         )
-            #         stats['vectordb']['inserted'] += 1
-            #         logger.info(f"Inserted: {file_num}, {page}, {paragraph}: {result}")
-            #
-
-        # logger.info(f"index stats:\n{pformat(idx.get_stats())}")
     return file_num
 
 
@@ -244,14 +224,13 @@ def main():
 
     # Statistics
     stats: dict = {
-        'page': defaultdict(int),
-        'vectordb': defaultdict(int),
-        'mongo': defaultdict(int),
-        'fs': defaultdict(int),
-        'file': defaultdict(int),
+        'vectordb': defaultdict(int),   # Inserts into vector db
+        'mongo': defaultdict(int),      # Inserts into mongodb
+        'fs': defaultdict(int),         # File system. If .txt file written
+        'file': defaultdict(int),       # Individual file statistics
     }
 
-    count: int = parse(stats)
+    parse(stats)
 
     logger.info(get_stats(stats))
     logger.info("Fetcher finished")
